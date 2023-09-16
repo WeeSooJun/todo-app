4a7fb62247d4e25a567b4e6e93fb59d8df3f240d
WeeSooJun <weesj96@gmail.com>
2023-09-16 22:42:58 +0800

    add get, post, patch and delete placeholders

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route(
+        "/todos",
+        get(|| async { "Todo 1 Todo 2" })
+            .post(|| async { "Todo added!" })
+            .patch(|| async { "Todo updated!" })
+            .delete(|| async { "Todo deleted!" }),
+    );
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    // run it with hyper on localhost:8080
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
