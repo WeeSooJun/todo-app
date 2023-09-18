b3e0e1e0759863ac64cedaf261990887619e17ec
WeeSooJun <weesj96@gmail.com>
2023-09-18 22:40:49 +0800

    add in connection and trying to println

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use axum::{routing::get, Router};
+use sqlx::{postgres::PgPoolOptions, Row};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://username:password@localhost/todo-app")
+        .await?;
+
+    let rows = sqlx::query("SELECT * FROM valid_todo_state")
+        .fetch_all(&pool)
+        .await?;
+    for row in rows.iter() {
+        println!("{:?}", row.get::<String, &str>("valid_state"));
+    }
+
     // build our application with a single route
     let app = Router::new().route(
         "/todos",
@@ -16,4 +29,6 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
