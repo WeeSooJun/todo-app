cdc167220b29b095d6993a89c07ff7434433c830
WeeSooJun <weesj96@gmail.com>
2023-09-19 21:33:39 +0800

    change to end_datetime instead of just end_date

--- sql/todo.sql
+++ sql/todo.sql
@@ -3,6 +3,6 @@ CREATE TABLE todo (
     id SERIAL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    end_date DATE,
+    end_date TIMESTAMP,
     todo_state TEXT NOT NULL REFERENCES valid_todo_state(valid_state) ON UPDATE CASCADE
 );
